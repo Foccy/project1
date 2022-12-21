82cf3c8dbb2584f945746fec9fc78ffe491cf47e
Foccy <pouneun@naver.com>
2022-12-21 21:32:31 +0900

    메인수정

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// document.addEventListener("DOMContentLoaded", function () {
-// 	document.querySelector("#layer button").addEventListener("click", function () {
-// 		document.querySelector("#layer").style.display = "none";
-// 	});
-// });
+document.addEventListener("DOMContentLoaded", function () {
+	document.querySelector("#layer button").addEventListener("click", function () {
+		document.querySelector("#layer").style.display = "none";
+	});
+});
 $(function(){
   $(".list1>h2 .dep2").mouseover(function(){
     $(".list1>h2").stop().slideDown(600);

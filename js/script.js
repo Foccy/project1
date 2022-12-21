dc575b2511cf53c1c63f644efc8aff1212d5bb5a
Foccy <pouneun@naver.com>
2022-12-21 17:27:49 +0900

    수정

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-document.querySelector("#layer button").addEventListener("click", function () {
-  document.querySelector("#layer").style.display = "none";
+// document.addEventListener("DOMContentLoaded", function () {
+// 	document.querySelector("#layer button").addEventListener("click", function () {
+// 		document.querySelector("#layer").style.display = "none";
+// 	});
+// });
+$(function(){
+  $(".list1>h2 .dep2").mouseover(function(){
+    $(".list1>h2").stop().slideDown(600);
+  });
+  $(".list1>h2 .dep2").mouseleave(function(){
+    $(".list1>h2").stop().slideUp(600);
+  });
 });

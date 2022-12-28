81bc54f1a0cc5edead98c38e86a0699b12439fa6
Foccy <pouneun@naver.com>
2022-12-29 00:58:59 +0900

    mainslide autoplay script

--- js/script.js
+++ js/script.js
@@ -39,7 +39,45 @@ $(".dep1_0 li, .header_bottom_2")
   .mouseout(function () {
     $(".header_bottom_2").stop().slideUp(0);
   });
+//------------------------------------------------------
 
+$(".dep1_0_1, .script1")
+  .mouseover(function () {
+    $(".script1").css("background", "#f5f5f5");
+  })
+  .mouseout(function () {
+    $(".script1").css("background", "white");
+  });
+$(".dep1_0_2, .script2")
+  .mouseover(function () {
+    $(".script2").css("background", "#f5f5f5");
+  })
+  .mouseout(function () {
+    $(".script2").css("background", "white");
+  });
+$(".dep1_0_3, .script3")
+  .mouseover(function () {
+    $(".script3").css("background", "#f5f5f5");
+  })
+  .mouseout(function () {
+    $(".script3").css("background", "white");
+  });
+$(".dep1_0_4, .script4")
+  .mouseover(function () {
+    $(".script4").css("background", "#f5f5f5");
+  })
+  .mouseout(function () {
+    $(".script4").css("background", "white");
+  });
+$(".dep1_0_5, .script5")
+  .mouseover(function () {
+    $(".script5").css("background", "#f5f5f5");
+  })
+  .mouseout(function () {
+    $(".script5").css("background", "white");
+  });
+
+// -----------------------------------
 var i = 0,
   k = null,
   repeat;
@@ -55,6 +93,6 @@ function timer() {
   }, 5000);
 }
 function slide() {
-  $(".main_slide_img li").eq(i).addClass("on");
-  $(".main_slide_img li").eq(k).removeClass("on");
+  $(".main_slide_img li img").eq(i).addClass("on");
+  $(".main_slide_img li img").eq(k).removeClass("on");
 }

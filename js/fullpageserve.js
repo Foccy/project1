779c2a507cc84dafd02b8647f0421b7bb82104a1
Foccy <pouneun@naver.com>
2023-02-07 01:03:24 +0900

    scroll modify

--- js/fullpageserve.js
+++ js/fullpageserve.js
@@ -1,18 +1,20 @@
-$(document).ready(function () {
-  $("#fullpage").fullpage({
-    menu: "#menu",
-    anchors: ["brandVisual", "second_slide", "third_slide", "fourth_slide"],
-    sectionsColor: ["#ffffff", "#ffffff", "#ffffff", "#ffffff"],
-    navigation: true,
-    navigationPosition: "left",
-    afterLoad: function (obj, idx) {
-      var activeMenu;
-      if (idx == 4) {
-        activeMenu = $("#fp-nav")
-          .find("li")
-          .eq(idx - 1)
-          .addClass("on");
-      }
-    },
-  });
-});
+
+      $(document).ready(function () {
+        $("#fullpage").fullpage({
+          menu: "#menu",
+          anchors: ["section1", "section2", "section3", "section4"],
+          sectionsColor: ["#f2f2f2", "#fff", "#7BAABE", "#fff"],
+          navigation: true,
+          navigationPosition: "right",
+          navigationTooltips: ["firstSlide", "secondSlide"],
+          afterLoad: function (obj, idx) {
+            var activeMenu;
+            if (idx == 4) {
+              activeMenu = $("#fp-nav")
+                .find("li")
+                .eq(idx - 1)
+                .addClass("on");
+            }
+          },
+        });
+      });
